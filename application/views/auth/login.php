ad3af768c17e93f8e7178d5da880f89da4836755
aryoferdyan <63910862+aryoferdyan@users.noreply.github.com>
2021-12-14 17:06:54 +0700

    s

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -71,15 +71,8 @@
             <div class="row" style="margin-top: 20px;">
                 <div class="col-xs-12">
                     <div class="callout callout-info">
-                        <h4>Level Super Admin</h4>
-
-                        <p><b>Email</b> : nuris.akbar@gmail.com</p>
-                        <p><b>Password</b> : password</p>
-                        <hr>
-                        <h4>Level Admin</h4>
-
-                        <p><b>Email</b> : hafid@gmail.com</p>
-                        <p><b>Password</b> : password</p>
+                        <p><b>Email</b> : aryoferdyan@gmail.com</p>
+                        <p><b>Password</b> : qwerty</p>
                     </div>
                 </div>
 
