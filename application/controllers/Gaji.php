767c2c92722c4426e64bec881f046ebe862bf007
aryoferdyan <63910862+aryoferdyan@users.noreply.github.com>
2021-12-14 21:12:37 +0700

    update

---
+++ application/controllers/Gaji.php
@@ -0,0 +1,22 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Gaji extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        is_login();
+       // $this->load->model('Karyawan_model');
+        $this->load->library('form_validation');        
+	//$this->load->library('datatables');
+    }
+
+    public function index()
+    {
+        $this->template->load('template','gaji/index.php');
+    } 
+    
+}
---
+++ application/views/cuti/cuti_read.php
@@ -0,0 +1,51 @@
+
+<div class="content-wrapper">
+	
+	<section class="content">
+		<div class="box box-warning box-solid">
+			<div class="box-header with-border">
+				<h3 class="box-title">DETAIL DATA CUTI</h3>
+			</div>
+		
+		<table class='table table-bordered'>        
+
+	
+			<tr>
+				<td>Id Karyawan</td>
+				<td><?php echo $id_karyawan; ?></td>
+			</tr>
+	
+			<tr>
+				<td>Tanggal1</td>
+				<td><?php echo $tanggal1; ?></td>
+			</tr>
+	
+			<tr>
+				<td>Tanggal2</td>
+				<td><?php echo $tanggal2; ?></td>
+			</tr>
+	
+			<tr>
+				<td>Id Jenis</td>
+				<td><?php echo $id_jenis; ?></td>
+			</tr>
+	
+			<tr>
+				<td>Status</td>
+				<td><?php echo $status; ?></td>
+			</tr>
+	
+			<tr>
+				<td>Validasi</td>
+				<td><?php echo $validasi; ?></td>
+			</tr>
+	
+			<tr>
+				<td></td>
+				<td><a href="<?php echo site_url('cuti') ?>" class="btn btn-default">Kembali</a></td>
+			</tr>
+	
+		</table>
+		</div>
+	</section>
+</div>
